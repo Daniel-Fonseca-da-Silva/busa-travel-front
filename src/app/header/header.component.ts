00d3d0bb2c6f6952860b2c1ac75521d94a8c791f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-02-26 01:33:47 -0300

    feat: Refactored the links and footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private readonly router: Router) {}
 
+  btnClick = () => this.router.navigateByUrl('/contato');
 }
